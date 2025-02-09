8d81a72ab1e2fced5ee203628a8a7cdba9613ae5
irfan-official <irfanofficial.work@gmail.com>
2025-02-09 22:55:58 +0600

    Ending JS class 4

--- JavaScript/JS_Lecture_4/pollyfils.js
+++ JavaScript/JS_Lecture_4/pollyfils.js
@@ -1,66 +1,7 @@
-const teas = {
-  name: "Lemon tea",
-  "tea type": "Green",
-  caffine: "low",
-};
-
-console.log(teas.name);
-console.log(teas["tea type"]);
-
-teas.origin = "Assam";
-teas.caffine = "Medium";
-
-delete teas["tea type"];
-console.log("origin" in teas);
-
-for (let key in teas) {
-  console.log(key + ":" + teas[key]);
-}
-
-const myTeas = {
-  greentea: {
-    name: "Green Tea",
-  },
-  blackTea: {
-    name: "Black tea",
-  },
-};
-
-let teaCopy = JSON.parse(JSON.stringify(myTeas));
-
-console.log(teaCopy);
-
-Object.prototype.chai = function () {
-  console.log("chai aur code");
-};
-const tea = {
-  name: "Ice tea lemon",
-  type: "cool",
-};
-tea.chai();
-
-tea.__proto__.checkItem = () => {
-  return true;
-};
-
-const show = {
-  message: "OK",
-};
-
-console.log(show.checkItem());
-
-const arr = [1, 2, 3];
-
-// prototye <---- .__proto__
-
-//polyfils
-
-// Array.prototype = {}
-// let arr = new Array([1,2,,3]) ---> arr.__proto__ = Array.prototype;
-const arr1 = [4];
+//1. polyfill for forEach
 
 if (!Array.prototype.myForEach) {
-  // Fallback - polyfill - Backup Function
+  // Fallback - Polyfill - Backup Function
   Array.prototype.fill = function () {
     const arr = [1, 2, 3, 4, 5, 6];
     // Error: -> .forEach function does not exist on arr variable or .forEach not defined
@@ -81,6 +22,7 @@ if (!Array.prototype.myForEach) {
   };
 }
 
+//2. polyfill for map
 //signature .map
 // Rety=turn? - New Array, Each ele Iterate, userFn
 
@@ -101,6 +43,7 @@ if (!Array.prototype.myMap) {
   };
 }
 
+//3. polyfill for filter
 // Filter
 // Signature: Return - new array | inpout: userFn,
 // agar user ka function True return karta hai toh current value ko new array mai return karta hai
