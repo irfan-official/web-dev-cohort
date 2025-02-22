5204c920833286a5c4eccd4e2f56026bfeea8acd
irfan-official <irfanofficial.work@gmail.com>
2025-02-22 21:09:28 +0600

    added datatypes

---
+++ JS_new_SERIES/01_variables_datatypes.js
@@ -0,0 +1,21 @@
+let name = "Irfan";
+const pi = 3.13;
+
+let number = 43; //Number
+let text = "Hello"; // String
+let isTrue = true; // Boolean
+let nothing = null; // Object
+let undefinedVar = undefined; // undefined
+let symbol = Symbol(); // symbol
+let bigInt = BigInt(100); // bigint
+
+let person = {
+  // Object
+  name: "Irfan",
+  age: 19,
+  isStudennt: true,
+};
+
+let num = "42";
+let convertedNum = Number(num);
+console.log(convertedNum);
