3785e3608b539aa97da469fd97c92c47fbb2853a
irfan-official <irfanofficial.work@gmail.com>
2025-02-24 23:59:55 +0600

    updated Promise.js

--- JavaScript/JS_PROJECT_SERIES/Day_01/06_Promises.js
+++ JavaScript/JS_PROJECT_SERIES/Day_01/06_Promises.js
@@ -2,3 +2,6 @@ const data = fetch("https://api.bd.irfans.dev");
 data.then((res) => res.json()).then((data) => console.log(data));
 
 // promise ---> pending --> either fulfill or reject
+
+// <fulfill, response>
+// <reject, ERROR>
