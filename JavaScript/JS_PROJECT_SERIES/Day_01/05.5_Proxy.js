29d7a9917186195d0517851bed32bfd04c4ab849
irfan-official <irfanofficial.work@gmail.com>
2025-02-25 19:17:33 +0600

    added Proxy.js

---
+++ JavaScript/JS_PROJECT_SERIES/Day_01/05.5_Proxy.js
@@ -0,0 +1,34 @@
+let arr = [1, 2, 3, 4, 5];
+
+function P_R_O_X_Y(object) {
+  return new Proxy(object, {
+    get(target, prop) {
+      let check = Number(prop);
+      if (typeof check == "number") {
+        if (prop < 0 && !prop in target) {
+          prop = `${target.length + check}`;
+          // console.log("Pass", prop);
+        }
+        return prop in target
+          ? target[prop]
+          : `Property "${prop}" does not exist`;
+      } else {
+        // this is a string prop
+        return prop in target
+          ? target[prop]
+          : `Property "${prop}" does not exist`;
+      }
+    },
+    set(target, prop, value) {
+      target[prop] = value;
+      return true;
+    },
+  });
+}
+
+let proxArr = P_R_O_X_Y(arr);
+
+proxArr[-5] = 1111;
+
+console.log(arr); // [ 1, 2, 3, 4, 5, '-5': 1111 ]
+console.log(proxArr[-5]);
--- JavaScript/JS_PROJECT_SERIES/Day_01/06_Promises.js
+++ JavaScript/JS_PROJECT_SERIES/Day_01/06_Promises.js
@@ -5,3 +5,16 @@ data.then((res) => res.json()).then((data) => console.log(data));
 
 // <fulfill, response>
 // <reject, ERROR>
+// if promise fullfill then we can .then()
+// if promise reject or ERROR then we can .catch()
+// .finally() will runn all time and its not depends on fullfill and reject
+// 3 state
+// 3_1 pending
+// 3_2 fullfill
+// 3_3 Reject
+// 3_4 setteled
+// 3_4_1 fullfill setteled
+// 3_4_2 Reject settele
+
+
+
