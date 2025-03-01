d00c26d53b3da1fae8ed3287eba2e92de7ab752c
irfan-official <irfanofficial.work@gmail.com>
2025-03-01 22:13:12 +0600

    Added Counter.js

---
+++ JavaScript/JS_Lecture_8/Counter.js
@@ -0,0 +1,38 @@
+let count = 0;
+function increment() {
+  // Clouser Function (Function binded by its lexical scope)
+  return () => count++;
+}
+
+let val = increment();
+
+console.log(val());
+console.log(val());
+console.log(val());
+console.log(val());
+console.log(val());
+console.log(val());
+
+val = null; // we use val = undefined as well as
+// solve the memory leack problems
+// clear the memory refference
+
+function change(_, val = 111) {
+  z = val;
+}
+
+let z = 999;
+change(z, 111);
+console.log(z);
+
+// Debounce is use clouser
+function createDebounce(cb, delay = 1) {
+  let timerId = null;
+
+  return function () {
+    clearTimeout(timerId);
+    timerId = setTimeout(cb, delay * 1000);
+  };
+}
+
+//
