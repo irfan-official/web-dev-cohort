96cb1d8e58f7603d32a558075880fafdca312d21
irfan-official <irfanofficial.work@gmail.com>
2025-03-09 20:35:12 +0600

    add changes

--- PROJECT_SERIES/controllers/user.controllers.js
+++
@@ -1,7 +0,0 @@
-import User from "../models/User.models.js";
-export const registerUser = async (req, res) => {
-  res.send("registered");
-};
-export const login = async (req, res) => {
-  res.send("registered");
-};
--- PROJECT_SERIES/routes/user.routes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { registerUser } from "../controllers/user.controllers.js";
-const router = express.Router();
-
-export default router;
-
-router.get("/register", registerUser);
